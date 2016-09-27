51f3c98dadee8b7d2070907794a5e31d355ba25e
Arnab Bir <arnabbir@gmail.com>
2016-09-28 02:49:27 +0530

    Update TouristGuide.java

--- src/TouristGuide.java
+++ src/TouristGuide.java
@@ -18,10 +18,10 @@ public class TouristGuide extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Calibri", 1, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Calibri", 1, 24));
         jLabel1.setText("Tourist Guide");
 
-        jLabel2.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Calibri", 0, 18));
         jLabel2.setText("Welcome to Siliguri");
 
         jButton1.setText("Tourists Spots");
@@ -132,7 +132,7 @@ public class TouristGuide extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>                        
+    }                        
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         TouristsSpots tp = new TouristsSpots();
@@ -174,15 +174,9 @@ public class TouristGuide extends javax.swing.JFrame {
         this.setVisible(false);
     }                                        
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -199,9 +193,7 @@ public class TouristGuide extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TouristGuide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TouristGuide().setVisible(true);
@@ -209,7 +201,6 @@ public class TouristGuide extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -219,5 +210,4 @@ public class TouristGuide extends javax.swing.JFrame {
     private javax.swing.JButton jButton7;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    // End of variables declaration                   
 }
